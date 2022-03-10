7bd374c3e7a89ce7acd424460be360825e9f513d
yusramn <yusramn63@gmail.com>
2022-03-10 13:40:39 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char: %d byte(s)\n", sizeof(char));
+	printf("size of int: %d byte(s)\n", sizeof(int));
+	printf(size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", size(float));
+	return(0);
+}
+
+
+
+
