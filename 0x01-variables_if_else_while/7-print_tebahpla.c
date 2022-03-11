1ed3554603adc937ea1a351f6326cbfd45e7be90
yusramn <yusramn63@gmail.com>
2022-03-11 12:02:38 -0800

    7-print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 122; i > 9; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0;)
+}
