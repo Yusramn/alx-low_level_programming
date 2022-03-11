add3bd703ff9139b479a0380042beba246944ae7
yusramn <yusramn63@gmail.com>
2022-03-11 10:38:41 -0800

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints the lowcase alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpa[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
