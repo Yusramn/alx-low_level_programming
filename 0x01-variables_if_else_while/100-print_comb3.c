bdbd2a0a0eb3a498a2fda82cb053dab8d5749821
yusramn <yusramn63@gmail.com>
2022-03-12 04:21:18 -0800

    100-prints

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int i, e;
 
-	i= 48;
-	e= 48;
+	i = 48;
+	e = 48;
 
 	while (e < 58)
 	{
-		i=48;
+		i = 48;
 		while (i < 58)
 		{
-			if(e != i && e < i)
+			if (e != i && e < i)
 			{
 		putchar(e);
 		putchar(i);
