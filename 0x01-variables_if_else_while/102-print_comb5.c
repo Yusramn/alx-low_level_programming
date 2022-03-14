54e8497be29df2afeda0ffb3232b83a5efd613e5
yusramn <yusramn63@gmail.com>
2022-03-14 01:11:11 -0700

    102-print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,8 +21,8 @@ int main(void)
 				i = 48;
 				while (i < 58)
 				{
-					op1 =(h * 10) + g;
-					op2 =(e * 10) + i;
+					op1 = (h * 10) + g;
+					op2 = (e * 10) + i;
 					if (op1 < op2)
 					{
 						putchar(h);
