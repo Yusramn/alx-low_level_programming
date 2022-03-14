24be635cd1712ae2f27fc04017a5acbee7d10e43
yusramn <yusramn63@gmail.com>
2022-03-14 16:05:15 -0700

    101-print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,25 +12,25 @@ int main(void)
 	e = 48;
 	g = 48;
 
-	while(e < 58)
+	while (e < 58)
 	{
 		i = 48;
-		while(i < 58)
+		while (i < 58)
 		{
 			g = 48;
 		       while (g < 58)
 		{
 	if(e != i && e != g && i != g && e < i && i < g)
 	{
-putchar(e);
-putchar(i);
-putchar(g);
-if(i == 56 && e == 55 && g == 57)
+putchar (e);
+putchar (i);
+putchar (g);
+if (i == 56 && e == 55 && g == 57)
 {
 	break;
 }
-	putchar(',');
-		putchar(' ');
+	putchar (',');
+		putchar (' ');
 		}
 g++;
 }
