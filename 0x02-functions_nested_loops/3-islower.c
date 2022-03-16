dd0537da34da3594c0f69178739f9003c18cba56
yusramn <yusramn63@gmail.com>
2022-03-16 13:52:11 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isolwer - returns 1 if lowercase 0 otherwise
+ *
+ * Return: Always 0
+ */
+int -islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
