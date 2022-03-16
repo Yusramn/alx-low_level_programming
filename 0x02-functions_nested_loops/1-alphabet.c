cb637eac235a852565d589b3e17289010f49c3a6
yusramn <yusramn63@gmail.com>
2022-03-16 13:02:53 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,11 @@
 /**
  * print_alphabet - prints alphabet in lowercase
  *
- * */
+ */
 void print_alphabet(void)
 {
-	char c ;
-	
-	for (c = 'a'; c <= 'z'; c++)
+	char c;
+		for (c = 'a'; c <= 'z'; c++)
 	_putchar(c);
 	_putchar('\n');
 }
