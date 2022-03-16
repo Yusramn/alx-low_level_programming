3d0a3837cb9dcf7b8f042fe7d75191d60d8c5412
yusramn <yusramn63@gmail.com>
2022-03-16 14:17:08 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isolwer - returns 1 if lowercase 0 otherwise
- *
- * Return: Always 0
+ *@c: takes in a character
+ * Return:0 for uppercase,1 fo lowercase
  */
 int _islower(int c)
 {
