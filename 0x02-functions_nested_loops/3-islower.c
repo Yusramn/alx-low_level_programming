3e7d2737ca06f6d19c4b22c85a9ece04dfb72c9b
yusramn <yusramn63@gmail.com>
2022-03-16 14:24:34 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _isolwer - returns 1 if lowercase alphabet 0 otherwise
  *@c: takes in a character
- * Return:0 for uppercase,1 fo lowercase
+ * Return: 0 for uppercase,1 fo lowercase
  */
 int _islower(int c)
 {
