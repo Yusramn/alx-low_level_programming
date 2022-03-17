6e0fd8a782014ca28c21606b15632f8107b65dd4
yusramn <yusramn63@gmail.com>
2022-03-17 01:45:48 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - retun 1 if c is uppercase,return 0 other wise.
+ *@c - input function
+ * Return: int.
+ */
+int _isupper(int c);
+{
+	if(c >= 'A' && c <=z)
+	{
+		return(0)
+	}
+	else
+		return(1)
+}
