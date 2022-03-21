656168fcea844fae57cb88b81b239b7048027576
yusramn <yusramn63@gmail.com>
2022-03-21 15:34:07 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int: swap the values of two integers.
+ * @a: integer for swap.
+ * @b: integer for swap.
+ */ 
+ void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
+ 
+ 
