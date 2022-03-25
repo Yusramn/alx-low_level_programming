c5d74b214997b4b3b26cd2d7bb9c605d107c7e1e
yusramn <yusramn63@gmail.com>
2022-03-25 00:20:10 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,7 @@
 char *_strcat(char *dest, char *src);
 {
 	int x, y;
+
 	x = 0;
 	while (dest[x] !=  '\0')
 		x++;
@@ -17,8 +18,8 @@ char *_strcat(char *dest, char *src);
 	while (src[y]) != '\0')
 	{
 		dest[x] = src[y];
-			x++;
-			y++;
+		x++;
+		y++;
 	}
 	dest[x] = '\0';
 	return (dest);
