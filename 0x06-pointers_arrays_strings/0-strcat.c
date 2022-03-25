86b31899e5924f92034f31b65194932a74d8714b
yusramn <yusramn63@gmail.com>
2022-03-25 00:11:31 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,20 +6,20 @@
  *@src: string to append to dest
  * Return: address of dest
  */
+
 char *_strcat(char *dest, char *src);
 {
 	int x, y;
-
 	x = 0;
-	while (dest[x] !=  '/0')
+	while (dest[x] !=  '\0')
 		x++;
 	y = 0;
-	while (src[y]) ! = '/0')
+	while (src[y]) != '\0')
 	{
-		dest[x] = src[y]
+		dest[x] = src[y];
 			x++;
 			y++;
 	}
-	dest[x] = '/0';
+	dest[x] = '\0';
 	return (dest);
 }
