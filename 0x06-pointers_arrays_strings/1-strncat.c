161f7d915fac48d54731c2dac25db579060c9c12
yusramn <yusramn63@gmail.com>
2022-03-25 16:42:30 -0700

    1-str

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,16 +11,20 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	i = j = 0;
+	i = 0;
+	j = 0;
+
 	while (*(dest + i))
 		i++;
+
 	while (j < n && *(src + j))
 	{
 		*(dest + i) = *(src + j);
 		i++;
 		j++;
 	}
-	if (j < n)
-		*(dest + i) = *(src + j);
+	
+	dest[i]='\0';
+
 	return (dest);
 }
