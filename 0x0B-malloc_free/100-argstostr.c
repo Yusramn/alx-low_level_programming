77a7097c362acc5b32e20fe09786153a9c1fad2f
yusramn <yusramn63@gmail.com>
2022-04-05 14:28:32 -0700

    100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,9 +10,9 @@ char *argstostr(int ac, char **av)
 {
 	char *aout;
 	int c, i, j, ia;
-	if ( ac == 0 )
-	
-			return (NULL);
+	if (ac == 0)
+		return (NULL);
+
 	for (c = i = 0; i < ac; i++)
 	{
 		if (av[i] == NULL)
